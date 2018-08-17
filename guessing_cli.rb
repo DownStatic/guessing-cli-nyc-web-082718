bf491dcb31c3af5f689846c57ac275d17de5024f
Christopher Stull <chris.stull@gmail.com>
2018-08-17 15:24:37 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,3 +3,19 @@
 def computer_guess
   return rand(1..6)
 end
+
+def run_guessing_game
+  while input != "exit" do
+    puts "Guess a number 1 through 6:"
+    input = gets.chomp
+    guess = computer_guess
+    if input == computer_guess.to_s
+      puts "You guessed the correct number!"
+      elsif
+      input == "exit"
+      break
+    else
+      puts "The computer guessed #{guess}"
+    end
+  end
+end
